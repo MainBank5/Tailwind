7820091ade07611811c121cf5b8826af2d9bba4d
eliud <eliudkaruga97@gmail.com>
2023-01-25 19:50:21 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    spacing: {
+      '1': '200px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    }, 
+    extend: { 
+      colors:{
+      tahiti:{
+        300:'#03a1fc',
+        400: '#3425'
+      },
+    },
+    },
+  },
+  plugins: [],
+}
